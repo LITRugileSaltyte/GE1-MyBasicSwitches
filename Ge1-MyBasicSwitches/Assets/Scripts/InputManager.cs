05ccc5149f7ee139d60cf6bd7f409cde31ad510e
Rugile Saltyte <rugilesaltyte97@gmail.com>
2019-01-22 20:55:58 +0000

    Update InputManager.cs

--- Ge1-MyBasicSwitches/Assets/Scripts/InputManager.cs
+++ Ge1-MyBasicSwitches/Assets/Scripts/InputManager.cs
@@ -6,13 +6,24 @@ public class InputManager : MonoBehaviour {
 	// a little out of hand now. This is the 3rd script that has a reference to the Switch.
 	// What's going to happen when we have multiple Switches and Doors etc.....
 	public SwitchController theSwitch;
+    public BulbController theBulb;
 
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.Space) == true) { //has spacebar been pressed
 			if (theSwitch != null) { //the variable f the switch is equal to nothing (if it hadnt been wired up), if u made a mistake and didnt drag it over, call the function on the switchcontroller script
 				theSwitch.toggleSwitch (); //it would work perfectly only if u wired it up, this is just in case it hasnt 
-			}
+                if (theBulb.bulbOn == true)
+                {
+                    theBulb.turnOff();
+
+                }
+                else
+                {
+                    theBulb.turnOn();
+                }
+               
+            }
 		}
 	}
 }
